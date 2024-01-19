f2fd147b4917edd5c990410c4b33222f3b492ff4
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-19 17:33:21 +0530

    Result Formatting Function and Cash Register Checking

--- script.js
+++ script.js
@@ -16,3 +16,71 @@ const cash = document.getElementById('cash');
 const purchaseBtn = document.getElementById('purchase-btn');
 const priceScreen = document.getElementById('price-screen');
 const cashDrawerDisplay = document.getElementById('cash-drawer-display');
+// Function to format and display results
+const formatResults = (status, change) => {
+    displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
+    change.map(
+      money => (displayChangeDue.innerHTML += `<p>${money[0]}: $${money[1]}</p>`)
+    );
+    return;
+  };
+  // Function to check the cash register
+  const checkCashRegister = () => {
+    if (Number(cash.value) < price) {
+      alert('Customer does not have enough money to purchase the item');
+      cash.value = '';
+      return;
+    }
+  
+    if (Number(cash.value) === price) {
+      displayChangeDue.innerHTML =
+        '<p>No change due - customer paid with exact cash</p>';
+      cash.value = '';
+      return;
+    }
+  
+    let changeDue = Number(cash.value) - price;
+    let reversedCid = [...cid].reverse();
+    let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+    let result = { status: 'OPEN', change: [] };
+    let totalCID = parseFloat(
+      cid
+        .map(total => total[1])
+        .reduce((prev, curr) => prev + curr)
+        .toFixed(2)
+    );
+  
+    if (totalCID < changeDue) {
+      return (displayChangeDue.innerHTML = '<p>Status: INSUFFICIENT_FUNDS</p>');
+    }
+  
+    if (totalCID === changeDue) {
+      formatResults('CLOSED', cid);
+    }
+  
+    for (let i = 0; i <= reversedCid.length; i++) {
+      if (changeDue > denominations[i] && changeDue > 0) {
+        let count = 0;
+        let total = reversedCid[i][1];
+        while (total > 0 && changeDue >= denominations[i]) {
+          total -= denominations[i];
+          changeDue = parseFloat((changeDue -= denominations[i]).toFixed(2));
+          count++;
+        }
+        result.change.push([reversedCid[i][0], count * denominations[i]]);
+      }
+    }
+    if (changeDue > 0) {
+      return (displayChangeDue.innerHTML = '<p>Status: INSUFFICIENT_FUNDS</p>');
+    }
+  
+    formatResults(result.status, result.change);
+    updateUI(result.change);
+  };
+  
+  const checkResults = () => {
+    if (!cash.value) {
+      return;
+    }
+    checkCashRegister();
+  };

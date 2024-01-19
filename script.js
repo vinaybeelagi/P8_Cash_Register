c682ad185b340cf0f1e2acab295d11c00e74357a
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-19 17:36:01 +0530

    Update UI Function

--- script.js
+++ script.js
@@ -84,3 +84,33 @@ const formatResults = (status, change) => {
     }
     checkCashRegister();
   };
+// Function to update the user interface
+  const updateUI = change => {
+    const currencyNameMap = {
+      PENNY: 'Pennies',
+      NICKEL: 'Nickels',
+      DIME: 'Dimes',
+      QUARTER: 'Quarters',
+      ONE: 'Ones',
+      FIVE: 'Fives',
+      TEN: 'Tens',
+      TWENTY: 'Twenties',
+      'ONE HUNDRED': 'Hundreds'
+    };
+    // Update cid if change is passed in
+    if (change) {
+      change.forEach(changeArr => {
+        const targetArr = cid.find(cidArr => cidArr[0] === changeArr[0]);
+        targetArr[1] = parseFloat((targetArr[1] - changeArr[1]).toFixed(2));
+      });
+    }
+  
+    cash.value = '';
+    priceScreen.textContent = `Total: $${price}`;
+    cashDrawerDisplay.innerHTML = `<p><strong>Change in drawer:</strong></p>
+      ${cid
+        .map(money => `<p>${currencyNameMap[money[0]]}: $${money[1]}</p>`)
+        .join('')}  
+    `;
+  };
+  
